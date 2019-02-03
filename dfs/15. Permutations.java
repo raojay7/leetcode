e2901c6b79f0f597198d8c4f02428b8a7972f499
raojay7 <879645920@qq.com>
2019-02-03 09:25:35 +0800

    lintcode模板题

---
+++ dfs/15. Permutations.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    /*
+     * @param nums: A list of integers.
+     * @return: A list of permutations.
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        //只有当size为nums.length时才加入
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
+        if (nums==null||nums.length==0){
+            res.add(new ArrayList<>(list));
+            return res;
+        }
+        Arrays.sort(nums);
+        dfs(nums,res,list);
+        return res;
+    }
+
+    void dfs(int[] nums,List<List<Integer>> res,List<Integer> list){
+        if (list.size()==nums.length){
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if(list.contains(nums[i]))continue;
+            list.add(nums[i]);
+            dfs(nums,res,list);
+            list.remove(list.size()-1);
+        }
+    }
+}
